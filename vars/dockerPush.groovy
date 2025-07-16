a601a142481fe791d6ec68217f6c7c8dde51aed9
Sreyas Rajgounda Patil <sreyaspatil5@gmail.com>
2025-07-16 15:59:24 +0530

    Updated dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     def imageName   = config.imageName ?: error("Image name is required")
     def imageTag    = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'dockerHubCred'
+    def credentials = config.credentials ?: 'dockerhub-creds'
 
     echo "Pushing Docker image: ${imageName}:${imageTag}"
 
